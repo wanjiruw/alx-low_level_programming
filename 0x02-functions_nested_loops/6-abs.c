849a50dc8dbf833c9d09bdee510884a632cbf881
wanjiruw <wanjymacharia@gmail.com>
2023-06-21 14:20:19 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_abs(int) - check absolute value of an interger
+ *
+ *@n: input number as an integer
+ *
+ *Return: absolute value
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
