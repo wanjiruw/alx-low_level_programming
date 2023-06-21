aee99de1ce6ee3e7308739120e491adc2842670b
wanjiruw <wanjymacharia@gmail.com>
2023-06-20 17:32:27 -0700

    lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_alphabet - 'print alphabet in lowercase'
+ *
+ *Return: always zero
+ */
+
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 97; n <= 122; n++)
+
+	{
+		_putchar(n);
+	}
+
+	_putchar('\n');
+}
