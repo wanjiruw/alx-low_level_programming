f5a8f7e40bbd30018c89928ddd1f4c788bd6575f
wanjiruw <wanjymacharia@gmail.com>
2023-06-20 17:56:09 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - 'print the alphabet 10 times'
+ *
+ *Return: always zero
+ */
+
+void print_alphabet_x10(void)
+{
+	int n;
+	int k;
+
+	for (k = 1; n <= 10; n++)
+	{
+		_putchar(k);
+	}
+	_putchar('\n');
+}
