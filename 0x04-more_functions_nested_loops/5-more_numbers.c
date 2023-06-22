3083d40e75f3685e7136d7d1935194ffe784d200
wanjiruw <wanjymacharia@gmail.com>
2023-06-22 16:05:37 -0700

    straight line

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,11 +13,11 @@ void more_numbers(void)
 	{
 		for (n = 0; n <= 14; n++)
 		{
-			if (n > 9)
-			{
+			if (n >= 10)
+			
 
-				_putchar((n / 10) + '0');
-			}
+				_putchar('1');
+			
 			_putchar((n % 10) + '0');
 		}
 	}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - print a line
+ * @n: number of times n is printed
+ * Return: returns nothing
+ */
+
+void print_line(int n)
+{
+	while (n--)
+	{
+		_putchar('_');
+		break;
+	}
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+}
