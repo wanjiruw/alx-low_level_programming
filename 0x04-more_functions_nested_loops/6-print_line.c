5bf8ca33d8e172afa983a8209709e4393dcb3245
wanjiruw <wanjymacharia@gmail.com>
2023-06-22 16:14:57 -0700

    straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,13 +8,18 @@
 
 void print_line(int n)
 {
-	while (n--)
-	{
-		_putchar('_');
-		break;
-	}
 	if (n <= 0)
 	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
 		_putchar('\n');
 	}
 }
