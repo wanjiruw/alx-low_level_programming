0b6926b9ce29ba8261d3d25f1d7bb0e23fe88200
wanjiruw <wanjymacharia@gmail.com>
2023-06-26 18:27:44 -0700

    reverse gear

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,7 +4,6 @@
  *print_rev -  prints a string, in reverse
  *
  *@s: char
- *@c: char
  *
  *Return: string
  */
@@ -12,15 +11,13 @@
 void print_rev(char *s)
 {
 	int m;
-	char c;
 
 	for (m = 0; s[m] != 0; m++)
 	{
 	}
 	for (m = m - 1; m >= 0; m--)
 	{
-		c = s[m];
-			_putchar(c);
+		_putchar(s[m]);
 	}
 	_putchar('\n');
 }
