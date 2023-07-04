fdb548bc9ad0475cbc2cc784a98976c587ea71b3
wanjiruw <wanjymacharia@gmail.com>
2023-07-04 13:33:49 -0700

    memset

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -11,4 +11,5 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
 #endif
