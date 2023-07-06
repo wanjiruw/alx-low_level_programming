94124fedf527a5992b41aa7791ee4df8d9dee8fd
wanjiruw <wanjymacharia@gmail.com>
2023-07-06 11:18:39 -0700

    to dream

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+include "main.h"
+#include <string.h>
+/**
+ * _print_rev_recursion - print a string in reverse
+ * @s: string being printed
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
