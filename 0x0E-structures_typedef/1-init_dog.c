1b39ecac425d6a49bca973803e5d01739dbe6c9d
wanjiruw <wanjymacharia@gmail.com>
2023-07-18 16:18:52 -0700

    A dog is the only thing

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * init_dog - function thta initiliazes struct dog
+ * @d: structure being initialized
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+	else
+	{
+		return;
+	}
+}
