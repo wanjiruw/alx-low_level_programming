e7b21ceab842a2d4e79a23b52448f8cb5e65ad85
wanjiruw <wanjymacharia@gmail.com>
2023-09-14 13:57:45 -0700

    task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,8 +1,7 @@
 #include "lists.h"
-
 /**
- *print_list - prints elements of a list
- *@h: pointer to the list
+ *print_dlistint - prints elements of a list
+ *@h : pointer to the lists
  *Return: number of nodes
  */
 
@@ -11,7 +10,6 @@ size_t print_dlistint(const dlistint_t *h)
 	size_t count = 0;
 
 	while (h != NULL)
-
 	{
 		printf("%d\n", h->n);
 		h = h->next;
