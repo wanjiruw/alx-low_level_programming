0bc8d1a77174380e05f85b5f6099d5b429e0ecf9
wanjiruw <wanjymacharia@gmail.com>
2023-09-14 13:11:33 -0700

    task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,15 +6,14 @@
  *Return: number of nodes
  */
 
-size_t print_dlistint(const dlistint_t *h);
-
+size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
 
 	while (h != NULL)
 
 	{
-		printf("%d\n", h->);
+		printf("%d\n", h->n);
 		h = h->next;
 		count++;
 	}
