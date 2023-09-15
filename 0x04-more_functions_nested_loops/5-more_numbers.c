1e2c60ff4051231b7616c79eba04ccd5dcb6b19c
wanjiruw <wanjymacharia@gmail.com>
2023-09-15 16:02:03 -0700

    palindrome

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,14 +9,14 @@ void more_numbers(void)
 {
 	int i, n;
 
-	for (i = 1; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (n = 0; n <= 14; n++)
 		{
 			if (n >= 10)
 
 
-				_putchar('1');
+				_putchar((n / 10) + '0');
 
 			_putchar((n % 10) + '0');
 		}
