93e5f506e50e233b3bf06fe93aea4ee21c614bcf
wanjiruw <wanjymacharia@gmail.com>
2023-09-15 15:58:10 -0700

    task8

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -17,6 +17,7 @@ struct dlistint_s
 	struct dlistint_s *next;
 };
 typedef struct dlistint_s dlistint_t;
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
